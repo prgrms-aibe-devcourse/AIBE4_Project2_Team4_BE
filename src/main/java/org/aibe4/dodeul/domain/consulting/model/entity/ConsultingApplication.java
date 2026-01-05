fd04f3cf7c114d94563eb72641e3940f31c6587e
qhrtn002-design <qhrtn002@gmail.com>
2026-01-05 17:29:48 +0900

    refactor: 상담 태그(ConsultingTag) 리팩토링 및 패키지 분리
    ConsultingType을 ConsultingTag로 명칭 변경
    model.enums 패키지 생성 및 이동 (팀 컨벤션 적용)
    확정된 태그 항목 6종 반영 (CAREER, RESUME, PORTFOLIO 등)
    ConsultingApplication 엔티티 필드 수정
    
    #3

--- src/main/java/org/aibe4/dodeul/domain/consulting/model/entity/ConsultingApplication.java
+++ src/main/java/org/aibe4/dodeul/domain/consulting/model/entity/ConsultingApplication.java
@@ -6,36 +6,36 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.aibe4.dodeul.domain.common.model.entity.BaseEntity;
+import org.aibe4.dodeul.domain.consulting.model.enums.ConsultingTag;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "consulting_applications")
-public class ConsultingApplication extends BaseEntity { // BaseEntity 상속 필수!
-
+public class ConsultingApplication extends BaseEntity {
 
     @Column(name = "mentee_id", nullable = false)
     private Long menteeId;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false)
     private String title;
 
-    @Column(columnDefinition = "TEXT", nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    @Column(name = "file_url", columnDefinition = "TEXT")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "consulting_tag", nullable = false) // 컬럼명 변경
+    private ConsultingTag consultingTag; // 타입 및 변수명 변경
+
+    @Column(name = "file_url")
     private String fileUrl;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 20)
-    private ConsultingType consulting;
-
     @Builder
-    public ConsultingApplication(Long menteeId, String title, String content, String fileUrl, ConsultingType consulting) {
+    public ConsultingApplication(Long menteeId, String title, String content, ConsultingTag consultingTag, String fileUrl) {
         this.menteeId = menteeId;
         this.title = title;
         this.content = content;
+        this.consultingTag = consultingTag;
         this.fileUrl = fileUrl;
-        this.consulting = consulting;
     }
 }
--- src/main/java/org/aibe4/dodeul/domain/consulting/model/entity/ConsultingType.java
+++
@@ -1,7 +0,0 @@
-package org.aibe4.dodeul.domain.consulting.model.entity;
-
-public enum ConsultingType {
-    CAREER,     // 진로
-    PORTFOLIO,  // 포트폴리오
-    ETC         // 기타
-}
