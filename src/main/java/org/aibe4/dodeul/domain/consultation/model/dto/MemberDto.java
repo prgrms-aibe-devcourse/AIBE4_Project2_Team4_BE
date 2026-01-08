42cf82ee6d65269ce1e9c7694209fda0bd2d7756
JunHyun <junhyun1001@gmail.com>
2026-01-09 01:41:57 +0900

    refactor: 클래스명 변경 및 id 추가
    - 클라이언트에서 roomId와 memberId 사용
    
    #48

--- src/main/java/org/aibe4/dodeul/domain/consultation/model/dto/MemberDto.java
+++ src/main/java/org/aibe4/dodeul/domain/consultation/model/dto/MemberDto.java
@@ -6,20 +6,22 @@ import org.aibe4.dodeul.domain.member.model.entity.Member;
 
 @Getter
 @Builder
-public class OpponentMemberDto {
+public class MemberDto {
 
+    private Long memberId;
     private String nickname;
     private String profileUrl;
 
-    public static OpponentMemberDto of(Member member) {
+    public static MemberDto of(Member member) {
         if (member == null) {
-            return OpponentMemberDto.builder()
+            return MemberDto.builder()
                     .nickname("(알 수 없음)")
                     //                .profileUrl(null) // 프로필 이미지가 없을 때 기본 이미지 설정 필요
                     .build();
         }
 
-        return OpponentMemberDto.builder()
+        return MemberDto.builder()
+                .memberId(member.getId())
                 .nickname(member.getNickname())
                 //            .profileUrl(member.getProfileUrl())
                 .build();
