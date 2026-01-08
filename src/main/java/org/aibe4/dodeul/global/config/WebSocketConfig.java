d756b185f49de64ae911e38a3d5be1d3815a5124
JunHyun <junhyun1001@gmail.com>
2026-01-09 01:37:37 +0900

    refactor: websocket 연결 경로 변경 및 클래스명 변경
    #48

--- src/main/java/org/aibe4/dodeul/global/config/WebSocketConfig.java
+++ src/main/java/org/aibe4/dodeul/global/config/WebSocketConfig.java
@@ -8,18 +8,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/consultation") // ws://consultation 경로로 연결 시도
-            .withSockJS()
-            .setHeartbeatTime(60_000); // 1분마다 확인하여 서버가 메시지를 보내지 않을 때 자동으로 클라이언트와 연결을 종료함
+        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("topic", "/queue");
+        registry.enableSimpleBroker("/topic", "/queue");
     }
 }
